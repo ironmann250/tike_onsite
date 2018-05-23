98e852180f60a04afd005c353a877fadfe0fb4c9
ironmann250 <ironmann350@outlook.com>
2018-05-24 01:08:44 +0800

    added static files

--- tikecom/settings.py
+++ tikecom/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 LOGIN_REDIRECT_URL = '/sell'
 LOGIN_URL = '/login/'
 #---- add/remove this section to not use dev{heroku} db 3------
