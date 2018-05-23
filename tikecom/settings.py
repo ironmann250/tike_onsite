35b6968a6472cb25abf58bb4e4e1c9ee2c13df96
ironmann250 <ironmann350@outlook.com>
2018-05-24 01:50:19 +0800

    added static files

--- tikecom/settings.py
+++ tikecom/settings.py
@@ -124,23 +124,24 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/sell'
 LOGIN_URL = '/login/'
 #---- add/remove this section to not use dev{heroku} db 3------
-'''
+
 import dj_database_url
 DATABASES ={}
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-'''
+
 #------------------------------------------------------------
 
 ALLOWED_HOSTS = ['*']
 
 DEBUG = True
 #add/remove this to use local db--------
+'''
 
 try:
     from .local_settings import *
 except ImportError:
     pass
-
+'''
 #---------------------------------------
