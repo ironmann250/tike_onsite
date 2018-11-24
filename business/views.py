d88e91fd8686462fd8a7ae1db9fea6436c6f62fa
Roberto Berwa <robertoberwa@dhcp-18-30-20-235.dyn.mit.edu>
2018-11-24 11:18:48 -0500

    f

--- business/views.py
+++ business/views.py
@@ -5,7 +5,7 @@ def create_db(request):
     return render(request,'html/badges/Create_database.html')
 
 def search(request):
-    return render(request,'html/badges/Create_database.html')
+    return render(request,'html/badges/search.html')
 
 def generate(request):
-     return render(request,'html/badges/Create_database.html')
+     return render(request,'html/badges/add_photo.html')
