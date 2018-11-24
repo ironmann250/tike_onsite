9a9a1f1f76076d75eb835eb43f93941f58449888
Roberto Berwa <robertoberwa@dhcp-18-30-20-235.dyn.mit.edu>
2018-11-24 10:35:58 -0500

    new fixes

--- business/views.py
+++ business/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def create_db(request):
-    return render(request,'html/badges/create_database.html')
+    return render(request,'html/badges/Create_database.html')
 
 def search(request):
-    return render(request,'html/badges/create_database.html')
+    return render(request,'html/badges/Create_database.html')
 
 def generate(request):
-     return render(request,'html/badges/create_database.html')
+     return render(request,'html/badges/Create_database.html')
